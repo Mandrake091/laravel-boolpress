91b295841eb64e09f3bf8098fffc1b491d4992f2
Mandrake091 <wasdgamer91@gmail.com>
2022-06-27 20:47:10 +0200

    show images in card.

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="card" style="width: 18rem;">
-                {{-- <img src="..." class="card-img-top" alt="..."> --}}
+                 <img src="{{asset('storage/'.$post->image)}}" class="card-img-top" alt="..."> 
                 <div class="card-body">
                     <h5 class="card-title">Titolo: {{ $post->title }}</h5>
                     <p class="card-text" value="{{$post->content}}">Contenuto: {!! $post->content !!}</p>
